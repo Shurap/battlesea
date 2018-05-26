b83e696cd37187c94b44a2f1336afc4901757d89
Shur <bap.essp@mail.ru>
2018-05-26 13:01:52 +0300

    new

--- public/js/dataexchange.js
+++ public/js/dataexchange.js
@@ -1,35 +1,54 @@
 newGame.onclick = function() {
-  var socket = io.connect();
+    var socket = io.connect();
 
-  socket.on('hello', createGame);
+    socket.on('hello', createGame);
 
 //  var btn = document.createElement('button');
 //  document.getElementById('test').appendChild(btn);
 
 }
-//git push
+
+click = function() {
+    alert('Клик на ' + this.id);
+}
+btnBorder = function() {
+    this.style.borderColor = (this.style.borderColor === "red") ? "black" : "red";
+}
 
 function createGame(data) {
 
-  console.log(data);
-  this.emit('name', {name : userName.value, game : gameName.value});
+    document.getElementById('userName').style.display = "none";
+    document.getElementById('gameName').style.display = "none";
+    document.getElementById('newGame').style.display = "none";
+    document.getElementById('joinGame').style.display = "none";
 
-  //document.location.href = '/html/game.html';
-  window.location.href = '/html/game.html';
+    console.log(data);
+    this.emit('name', {name : userName.value, game : gameName.value});
 
-  console.log('1');
+    console.log('1');
 
-  for (let i = 0; i < 10; i++) {
-    for (let j = 0; j < 10; j++) {
-      let btnField = document.createElement('div');
-      btnField.id = String(i) + String(j);
-      btnField.className = 'btn';
-      //btnField.onclick = click;
-      btnField.addEventListener("click", click);
-      btnField.addEventListener("mouseenter", btnBorder);
-      btnField.addEventListener("mouseleave", btnBorder);
-      document.getElementById('field').appendChild(btnField);
+    for (let i = 0; i < 10; i++) {
+        for (let j = 0; j < 10; j++) {
+            let btnField = document.createElement('div');
+            btnField.className = 'btn';
+            //btnField.onclick = click;
+            btnField.addEventListener("click", click);
+            btnField.addEventListener("mouseenter", btnBorder);
+            btnField.addEventListener("mouseleave", btnBorder);
+            btnField.id = 'h' + String(i) + String(j);
+            document.getElementById('home').appendChild(btnField);
+        }
     }
-  }
-
+    for (let i = 0; i < 10; i++) {
+        for (let j = 0; j < 10; j++) {
+            let btnField = document.createElement('div');
+            btnField.className = 'btn';
+            //btnField.onclick = click;
+            btnField.addEventListener("click", click);
+            btnField.addEventListener("mouseenter", btnBorder);
+            btnField.addEventListener("mouseleave", btnBorder);
+            btnField.id = 'e' + String(i) + String(j);
+            document.getElementById('enemy').appendChild(btnField);
+        }
+    }
 }
