430211f52fff81550a5b1e9bc9c615eb3a917fb5
Shur <bap.essp@mail.ru>
2018-06-18 20:39:26 +0300

    clean up

--- public/js/dataexchange.js
+++ public/js/dataexchange.js
@@ -11,17 +11,87 @@ btnNewGame.onclick = function() {
   socket.on('beginGame', beginGame);
   socket.on('battle', battle);
   socket.on('test', test);
+};
+
+// Create game and player connection
+function createGame(data) {
+// Hide old elements and show new elements
+  document.getElementById('start').style.display = "none";
+// Send name and title of game to server
+  this.emit('name', {name : userName.value, game : gameName.value});
+  //phaseGame = '1';
 }
 
-// тест
-function test(data) {
-  console.log(data);
+//Begin game (Phase 1)
+function beginGame(){
+  // Hide old elements and show new elements
+  document.getElementById('home').style.display = "block";
+  createBtnField('home');
+  getTerminal('> Расставьте свои корабли');
+  phaseGame = '1';
 }
 
-// Кнопка "Готово"
+// Create field of buttons
+function createBtnField(divId) {
+  for (let i = 0; i < 10; i++) {
+    for (let j = 0; j < 10; j++) {
+      let btnField = document.createElement('div');
+      btnField.className = 'btn';
+      btnField.addEventListener("click", click);
+      btnField.addEventListener("mouseenter", btnBorder);
+      btnField.addEventListener("mouseleave", btnBackPicture);
+      btnField.id = divId.substr(0, 1) + String(i) + String(j);
+      btnField.parent = divId.substr(0, 1);
+      btnField.content = 'zero';
+      btnField.adress = btnField.id;
+      btnField.allowpress = (divId === 'enemy') ? true : false;
+      document.getElementById(divId).appendChild(btnField);
+    }
+  }
+}
+
+// Handler event - press button of field
+function click() {
+//Phase one - arrange ships on the field 'home'
+  if (phaseGame === '1') {
+    if (verifArray('ship', 'home') < 5) {
+      this.style.backgroundImage = (this.style.backgroundImage === '') ? 'url("../img/ship.jpg")' : '';
+      this.content = (this.content === 'zero') ? 'ship' : 'zero';
+    } else {
+      this.style.backgroundImage = '';
+      this.content = 'zero';
+    }
+  }
+//Phase two - set aim on the field 'enemy'
+  if ((phaseGame === '2') && (this.allowpress === true)) {
+    //this.textContent = 'O';
+    this.style.backgroundImage = 'url("../img/aimblack.png")';
+
+    if (postId) {document.getElementById(postId).style.backgroundImage = 'url("../img/ship.jpg")';}
+    //console.log('this ', this);
+    postId = this.id;
+  }
+  btnPicture = this.style.backgroundImage;
+}
+
+// Verification array of buttons for count... (what count, where find)
+function verifArray(data, parentDiv) {
+  let count = 0;
+  for (let i = 0; i < 10; i++) {
+    for (let j = 0; j < 10; j++) {
+      let index = parentDiv.substr(0, 1) + String(i) + String(j);
+      if (document.getElementById(index).content === data) {
+        count ++;
+      }
+    }
+  }
+  return count;
+}
+
+// Press button 'Enter'
 btnEnter.onclick = function () {
-// Сборка массива поля и отправка на сервер
-  if ((phaseGame === '1') && (verifArray('ship') === 5)) {
+// Create array button and send to server
+  if ((phaseGame === '1') && (verifArray('ship', 'home') === 5)) {
     let array = create2DArray(10, 10);
     for (let i = 0; i < 10; i++) {
       for (let j = 0; j < 10; j++) {
@@ -34,56 +104,13 @@ btnEnter.onclick = function () {
     document.getElementById('enemy').style.display = "block";
     createBtnField('enemy');
     phaseGame = '2';
-  } else if (phaseGame = '2') {
+
+  } else if (phaseGame === '2') {
     socket.emit('shoot', postId);
   }
-}
+};
 
-// Создание комнаты и подключение игроков
-function createGame(data) {
-  // скрыть старые, открыть новые элементы
-  document.getElementById('start').style.display = "none";
-  // передача имени и названия игры на сервер
-  this.emit('name', {name : userName.value, game : gameName.value});
-  phaseGame = '1';
-}
-
-// проверка на количество в кнопках
-function verifArray(data) {
-  let count = 0;
-  for (let i = 0; i < 10; i++) {
-    for (let j = 0; j < 10; j++) {
-      let index = String(i) + String(j);
-      if (document.getElementById(index).content === data) {
-        count ++;
-      }
-    }
-  }
-  return count;
-}
-
-// обработчик нажатия на кнопку поля
-function click() {
-  //расстановка кораблей
-  if (phaseGame === '1') {
-    if (verifArray('ship') < 5) {
-      this.style.backgroundImage = (this.style.backgroundImage === '') ? 'url("../img/ship.jpg")' : '';
-      this.content = (this.content === 'zero') ? 'ship' : 'zero';
-    } else {
-      this.style.backgroundImage = '';
-      this.content = 'zero';
-    }
-  }
-  //установка прицела
-  if ((phaseGame === '2') && (this.allowpress === true)) {
-    //this.textContent = 'O';
-      this.style.backgroundImage = 'url("../img/aimblack.png")';
-    postId = this.id;
-  }
-  btnPicture = this.style.backgroundImage;
-}
-
-// создание пустого двумерного массива
+// Create empty 2D array
 function create2DArray(rows, columns) {
   let x = new Array(rows);
   for (let i = 0; i < rows; i++) {
@@ -92,7 +119,7 @@ function create2DArray(rows, columns) {
   return x;
 }
 
-// изменения кнопки поля при наведении
+// Change picture (phase 2) and border of buttons
 function btnBorder() {
   btnPicture = this.style.backgroundImage;
   this.style.borderColor = "red";
@@ -102,38 +129,19 @@ function btnBorder() {
   }
 }
 
+// Change back picture (phase 2) and border of buttons
 function btnBackPicture() {
   this.style.borderColor = "black";
   this.style.backgroundImage = btnPicture;
 }
 
-// сообщение в терминал
+// message to terminal
 function getTerminal(data) {
   document.getElementById('terminal').value += "\n" + data;
   document.getElementById('terminal').scrollTop = document.getElementById('terminal').scrollHeight;
 }
 
-// создание поля кнопок
-function createBtnField(divId) {
-  for (let i = 0; i < 10; i++) {
-    for (let j = 0; j < 10; j++) {
-      let btnField = document.createElement('div');
-      btnField.className = 'btn';
-      btnField.addEventListener("click", click);
-      btnField.addEventListener("mouseenter", btnBorder);
-      btnField.addEventListener("mouseleave", btnBackPicture);
-      btnField.id = String(i) + String(j);
-      btnField.content = 'zero';
-      //btnField.adress = btnField.id;
-      btnField.allowpress = (divId === 'enemy') ? true : false;
-      document.getElementById(divId).appendChild(btnField);
-    }
-  }
-}
-
-function beginGame(){
-  // скрыть старые, открыть новые элементы
-  document.getElementById('home').style.display = "block";
-  createBtnField('home');
-  getTerminal('> Расставьте свои корабли');
+// тест
+function test(data) {
+  console.log(data);
 }
--- server.js
+++ server.js
@@ -1,18 +1,13 @@
-var express = require('express');
-var app = express();
-//var path = require('path');
-var gamers = [];
-var gamersH =[];
-var usersId = [];
-var lobbyName;
-var numberGamer = '1';
-var arrField1 = [];
-
-
+let express = require('express');
+let app = express();
+let lobby;
+let objGamer1 = {};
+let objGamer2 = {};
+let lobbyName;
 
 
 //создание сервера
-var myServer = app.listen(process.env.PORT || 7777, function () {
+let myServer = app.listen(process.env.PORT || 7777, function () {
   console.log('Сервер запущен и слушает порт', process.env.PORT || 7777);
 });
 
@@ -23,7 +18,7 @@ app.get('/', function (req, res) {
 });
 
 //создание сокета на сервере
-var io = require('socket.io').listen(myServer);
+let io = require('socket.io').listen(myServer);
 
 // слушает сокет
 //io.sockets.on('connection', function (socket) {
@@ -39,20 +34,23 @@ io.on('connection', function (socket) {
 
 // создание комнаты и подключение игроков____________________
 function servCreateNewGame(data) {
-  lobbyName = data.game;
   let gamerName = data.name;
-  let lobby = this.adapter.rooms[lobbyName];
+  lobby = this.adapter.rooms[lobbyName];
   // создание комнаты
   if (lobby === undefined) {
   this.join(lobbyName);
-  this.emit('terminal', 'server> Игрок ' + gamerName + ' cоздал игру с именем - ' + lobbyName);
-  this.emit('terminal', 'server> Ждем подключения другого игрока...')
+  objGamer1['name'] = data.name;
+  objGamer1['id'] = this.id;
+  console.log('1 ', this.id);
   // присоединение к комнате
   } else if (lobby.length <= 1){
-    //console.log(lobby);
-    //console.log(this.id);
     this.join(lobbyName);
-    console.log(lobby);
+    objGamer2['name'] = data.name;
+    objGamer2['id'] = this.id;
+
+    console.log('2 ', this.id);
+    console.log('3 ', lobby);
+
     io.in(lobbyName).emit('terminal', 'server> Игрок ' + gamerName + ' подключился к игре с именем - ' + lobbyName);
     io.in(lobbyName).emit('beginGame');
   //комната заполнена
@@ -62,23 +60,23 @@ function servCreateNewGame(data) {
 }
 
 function servBeginGame(data) {
-  if (numberGamer === '1') {
-    const obj = {};
-    arrField1 = data.arr;
-    name1 = data.name;
-    obj[data.name] = data.arr; // привязать id вместо имени
-    //console.log(obj[data.name][1][1]);
-    numberGamer = '2';
-  } else if (numberGamer === '2') {
-    arrField2 = data.arr;
-    name2 = data.name;
-    numberGamer = '1';
-    //console.log(name1 + ' - ' + arrField1[0][0].content);
-    //console.log(name2 + ' - ' + arrField2[0][0].content);
-    this.emit('battle');
+  if (this.id === objGamer1.id) {
+    objGamer1['array'] = data.arr;
+  } else if (this.id === objGamer2.id) {
+    objGamer2['array'] = data.arr;
   }
-  this.broadcast.emit('test', 'ответ сервера');
-  //console.log('!!! ' + this.id);
+  if (objGamer1.array !== undefined) {
+    console.log('4.1 ', objGamer1.name, objGamer1.id, objGamer1.array[0][0]);
+    this.broadcast.emit('terminal', 'server> Игрок ' + objGamer2.name + ' расставил свои корабли');
+  }
+  if (objGamer2.array !== undefined) {
+    console.log('4.2 ', objGamer2.name, objGamer2.id, objGamer2.array[0][0]);
+    this.broadcast.emit('terminal', 'server> Игрок ' + objGamer1.name + ' расставил свои корабли');
+  }
+
+    //this.emit('battle');
+
+    //this.broadcast.emit('terminal', 'игрок расставил корабли');
 }
 
 function servVerifShoot(data) {
