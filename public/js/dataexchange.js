b0eef21e3e52768d1a1286090094bcfbd76aa549
Shur <bap.essp@mail.ru>
2018-06-21 22:08:57 +0300

    add function numberShip

--- public/js/dataexchange.js
+++ public/js/dataexchange.js
@@ -1,9 +1,9 @@
 let socket;
 let phaseGame;
-let name;
+let name;//---------------?
 let postId;
 let btnPicture;
-
+// Connect and listen server
 btnNewGame.onclick = function() {
   socket = io.connect();
   socket.on('connClient', createGame);
@@ -65,15 +65,15 @@ function click() {
 //Phase two - set aim on the field 'enemy'
   if ((phaseGame === '2') && (this.adress.substr(0, 1) === 'e')) {
     //this.textContent = 'O';
-    if (verifArray('aim', 'enemy') != 0) {
+    if (postId) {
       document.getElementById(postId).style.backgroundImage = '';
+      document.getElementById(postId).content = 'zero';
     }
     this.style.backgroundImage = 'url("../img/aimblack.png")';
-    this.content = 'aim';
     postId = this.id;
   }
   btnPicture = this.style.backgroundImage;
-  console.log(postId || this.id, ' ', btnPicture);
+  //console.log(postId || this.id, ' ', btnPicture, verifArray('aim', 'enemy'));
 
 }
 
@@ -99,7 +99,8 @@ btnEnter.onclick = function () {
     for (let i = 0; i < 10; i++) {
       for (let j = 0; j < 10; j++) {
         let index = String(i) + String(j);
-        array[i][j] = document.getElementById(index);
+        array[i][j] = document.getElementById('h' + index);
+        array[i][j].adress = array[i][j].adress.substr(1, 2);
       }
     }
     socket.emit('field', {name : userName.value, arr : array});
@@ -107,9 +108,9 @@ btnEnter.onclick = function () {
     document.getElementById('enemy').style.display = "block";
     createBtnField('enemy');
     phaseGame = '2';
-
+// Send to server shoot
   } else if (phaseGame === '2') {
-    socket.emit('shoot', postId);
+    socket.emit('shoot', postId.substr(1,2));
   }
 };
 
@@ -143,6 +144,8 @@ function getTerminal(data) {
   document.getElementById('terminal').value += "\n" + data;
   document.getElementById('terminal').scrollTop = document.getElementById('terminal').scrollHeight;
 }
+
+function battle(){}
 
 // тест
 function test(data) {
--- server.js
+++ server.js
@@ -1,28 +1,25 @@
 let express = require('express');
 let app = express();
-let lobby;
 let objGamer1 = {};
 let objGamer2 = {};
-let lobbyName;
 
-
-//создание сервера
+//Create server
 let myServer = app.listen(process.env.PORT || 7777, function () {
   console.log('Сервер запущен и слушает порт', process.env.PORT || 7777);
 });
 
-//передача страницы
+//Send site to client
 app.get('/', function (req, res) {
   app.use(express.static(__dirname + '/public'));
   res.sendFile(__dirname + '/public/html/index.html');
 });
 
-//создание сокета на сервере
+//Create socket on server
 let io = require('socket.io').listen(myServer);
 
-// слушает сокет
-//io.sockets.on('connection', function (socket) {
+//Listen client
 io.on('connection', function (socket) {
+//io.sockets.on('connection', function (socket) {
   //console.log('Подключился клиент');
   socket.emit('connClient');
   socket.emit('terminal', "server> подключились к серверу");
@@ -31,18 +28,18 @@ io.on('connection', function (socket) {
   socket.on('shoot', servVerifShoot);
 });
 
-
-// создание комнаты и подключение игроков____________________
+//Create room and connect gamers. Create objGamer with name, id
 function servCreateNewGame(data) {
+  let lobbyName = data.game;
   let gamerName = data.name;
-  lobby = this.adapter.rooms[lobbyName];
-  // создание комнаты
+  let lobby = this.adapter.rooms[lobbyName];
+//Create room and connect 1st
   if (lobby === undefined) {
   this.join(lobbyName);
-  objGamer1['name'] = data.name;
+  objGamer1['name'] = data.name;// .name
   objGamer1['id'] = this.id;
   console.log('1 ', this.id);
-  // присоединение к комнате
+//Connect to room 2nd
   } else if (lobby.length <= 1){
     this.join(lobbyName);
     objGamer2['name'] = data.name;
@@ -53,12 +50,13 @@ function servCreateNewGame(data) {
 
     io.in(lobbyName).emit('terminal', 'server> Игрок ' + gamerName + ' подключился к игре с именем - ' + lobbyName);
     io.in(lobbyName).emit('beginGame');
-  //комната заполнена
+//If room is full
   } else {
     this.emit('terminal', 'Уже подключились двое!');
-  } //__________________________________________________________
+  }
 }
 
+//Write to objGamer array
 function servBeginGame(data) {
   if (this.id === objGamer1.id) {
     objGamer1['array'] = data.arr;
@@ -67,11 +65,12 @@ function servBeginGame(data) {
   }
   if (objGamer1.array !== undefined) {
     console.log('4.1 ', objGamer1.name, objGamer1.id, objGamer1.array[0][0]);
-    this.broadcast.emit('terminal', 'server> Игрок ' + objGamer2.name + ' расставил свои корабли');
+    //this.broadcast.emit('terminal', 'server> Игрок ' + objGamer1.name + ' расставил свои корабли');
+
   }
   if (objGamer2.array !== undefined) {
     console.log('4.2 ', objGamer2.name, objGamer2.id, objGamer2.array[0][0]);
-    this.broadcast.emit('terminal', 'server> Игрок ' + objGamer1.name + ' расставил свои корабли');
+    //this.broadcast.emit('terminal', 'server> Игрок ' + objGamer2.name + ' расставил свои корабли');
   }
 
     //this.emit('battle');
@@ -81,4 +80,69 @@ function servBeginGame(data) {
 
 function servVerifShoot(data) {
   console.log ('shoot ', this.id, ' ', data);
+  let postTo = (this.id === objGamer1.id) ? objGamer2.id : objGamer1.id;
+  let target = (postTo === objGamer1.id) ? objGamer1.array : objGamer2.array;
+  let result = battle(data, target);
+
+  io.sockets.connected[postTo].emit('terminal', result);
+
+}
+
+function battle(data, target) {
+  let coordX = data.substr(0, 1);
+  let coordY = data.substr(1, 1);
+
+  if (target[coordX][coordY].content === 'ship') {
+
+    return numberShips(coordX, coordY, target);
+    //return 'hit';
+  } else {
+    return 'fail';
+  }
+
+}
+
+function numberShips(coordX, coordY, target) {
+  let count = 0;
+//vertical
+  for (let i = 0; i < 10; i++) {
+    if (i !== +coordX) {
+      if (target[i][coordY].content === 'ship') count++;
+    }
+  }
+//horizontal
+  for (let i = 0; i < 10; i++) {
+    if (i !== +coordY) {
+      if (target[coordX][i].content === 'ship') count++;
+    }
+  }
+  let i = coordX;
+  let j = coordY;
+  while ((i < 9) && (j < 9)) {
+    i++;
+    j++;
+    if (target[i][j].content === 'ship') count++;
+  }
+  i = coordX;
+  j = coordY;
+  while ((i > 0) && (j > 0)) {
+    i--;
+    j--;
+    if (target[i][j].content === 'ship') count++;
+  }
+  i = coordX;
+  j = coordY;
+  while ((i < 9) && (j > 0)) {
+    i++;
+    j--;
+    if (target[i][j].content === 'ship') count++;
+  }
+  i = coordX;
+  j = coordY;
+  while ((i > 0) && (j < 9)) {
+    i--;
+    j++;
+    if (target[i][j].content === 'ship') count++;
+  }
+    return count;
 }
